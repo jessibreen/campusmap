0c26a91b1aca69e7d41c88473ac9786445df5f37
jessibreen <jessibreen@gmail.com>
2017-01-17 23:45:26 -0500

    changed name to cartodb_id and removed "AND flag = false GROUP BY name ORDER BY name ASC"

--- js/hoodscript.js
+++ js/hoodscript.js
@@ -329,7 +329,7 @@ var hoodClickHandler = function(ev,latlng,pos,data){
 var hoodClickGetter = function(ll){
   getJSON.abort();
   getJSON = $.ajax(
-    {url:myPath+"/sql?q=SELECT name FROM "+tblName+" WHERE ST_Intersects( the_geom, ST_SetSRID((ST_POINT("+ll.lng+", "+ll.lat+")) , 4326)) AND flag = false GROUP BY name ORDER BY name ASC",
+    {url:myPath+"/sql?q=SELECT cartodb_id FROM "+tblName+" WHERE ST_Intersects( the_geom, ST_SetSRID((ST_POINT("+ll.lng+", "+ll.lat+")) , 4326)) ",
     crossDomain:true,
       dataType:"jsonp",
       error: function (xhr, text_status, error_thrown) {
@@ -385,7 +385,7 @@ var getNewHoods = function (arr){//takes array of neighborhood names, gets and d
        
         var newHoodCount = data.rows.length;
         if((currentHoodsCount+newHoodCount)>fillArr.length){
-          console.log('too many hoods!');
+          console.log('too many!');
           var diff = (currentHoodsCount+newHoodCount);
           var spliceOff = diff-fillArr.length;
             clearSomeHoods(spliceOff);
